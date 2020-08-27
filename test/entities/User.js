ec5886304a204e48f6a8c3d2d5f6e8c138be3c58
KhiemLuc <41050460+luckhiem@users.noreply.github.com>
2020-08-27 11:12:18 +0700

    update

---
+++ test/entities/User.js
@@ -0,0 +1,24 @@
+
+export default class User {
+    constructor(email, password) {
+        this._email = email;
+        this._password = password;
+    }
+
+    get email() {
+        return this._email;
+    }
+
+    set email(value) {
+        this._email = value;
+    }
+
+    get password() {
+        return this._password;
+    }
+
+    set password(value) {
+        this._password = value;
+    }
+}
+
--- test/test_scripts/example.e2e.js
+++ test/test_scripts/example.e2e.js
@@ -1,8 +1,11 @@
 import AuthFlow from '../test_flow/AuthFlow';
+import User from '../entities/User'
+
+const userTest = new User('khiemltv@gmail.com', 'khiemluc2903')
 
 describe('My Login application', () => {
     it('should login with valid credentials', () => {
-        const Auth = new AuthFlow('khiemltv@gmail.com', 'khiemluc2903')
+        const Auth = new AuthFlow(userTest.email, userTest.password)
         Auth
             .openLoginPage()
             .doLogin()
